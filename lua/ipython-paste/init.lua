1e9e6466853e859091cdade14ef3ee7af35fc4ee
Constantin Kühne <constantin@kuehne-mail.de>
2023-02-19 14:05:40 +0100

    extracted all tmux logic to own file

--- lua/ipython-paste/init.lua
+++ lua/ipython-paste/init.lua
@@ -1,94 +1,63 @@
+local tmux = require("ipython-paste.tmux")
+local text = require("ipython-paste.text")
+
 local M = {}
 
-local function get_tmux()
-    return os.getenv("TMUX")
+M.config = {
+    python_command = "ipython --no-banner",
+    cell_comment = "# %%"
+}
+
+M.pane = nil
+
+---Setup the ipython-paste plugin
+---Defaults are:
+---{
+---    python_command = "ipython --no-banner",
+---    cell_comment = "# %%"
+---}
+---@param opts { python_command: string, cell_comment: string }
+M.setup = function(opts)
+    M.config = opts
 end
 
-local function get_tmux_pane()
-    return os.getenv("TMUX_PANE")
-end
-
-local function get_socket()
-    return vim.split(get_tmux(), ",")[1]
-end
-
-local function execute(arg, pre)
-    local command = string.format("%s tmux -S %s %s", pre or "", get_socket(), arg)
-
-    local handle = assert(io.popen(command), string.format("unable to execute: [%s]", command))
-    local result = handle:read("*a")
-    handle:close()
-
-    return result
-end
-
-local function check_if_python(pane_candidate)
-    if pane_candidate.cur_cmd == "python" then
-        return true
+---Connect to tmux pane
+---@param pane_num number?
+M.connect = function(pane_num)
+    if not M.pane then
+        local pane = tmux.get_pane(pane_num)
+        if pane and not tmux.check_if_python(pane) then
+            tmux.run_python(pane, M.config.python_command)
+        end
+        M.pane = pane
     else
-        return false
+        vim.api.nvim_err_writeln(string.format("Please only connect with one pane. You are currenty connected to pane '%s'. First use the disconnect function to change pane."
+            , M.pane.index))
     end
 end
 
-local function get_pane_infos(cmd_out)
-    local pane_infos = {}
-
-    for _, pane_info in pairs(vim.split(cmd_out, "\n")) do
-        local info_splitted = vim.split(pane_info, " ")
-        if #info_splitted > 1 then
-            local index = tonumber(info_splitted[2])
-            local info_table = {
-                active = info_splitted[1],
-                index = info_splitted[2],
-                pid = info_splitted[3],
-                cur_cmd = info_splitted[4],
-                id = info_splitted[5]
-            }
-
-            if index then
-                table.insert(pane_infos, index + 1, info_table)
-            end
-        end
-    end
-    return pane_infos
+---Disconnect from tmux pane
+M.disconnect = function()
+    M.pane = nil
 end
 
-local function connect_to_tmux(pane_num)
-    pane_num = pane_num + 1
-
-    if get_tmux() then
-        local cmd_out = execute("list-panes -F '#{pane_active} #{pane_index} #{pane_pid} #{pane_current_command} #{pane_id}'"
-            , nil)
-
-        local pane_candidate = nil
-
-        local pane_infos = get_pane_infos(cmd_out)
-
-        if #pane_infos == 2 then
-            for _, pane_info in pairs(pane_infos) do
-                if pane_info.active == 0 then
-                    pane_candidate = pane_info
-                end
-            end
-        else
-            if pane_num == nil then
-                vim.api.nvim_err_writeln("Please specify the tmux pane number (to check use tmux-prefix + q)")
-            end
-            if pane_infos[pane_num].active ~= 1 then
-                pane_candidate = pane_infos[pane_num]
-            else
-                vim.api.nvim_err_writeln("pane num cannot be the active pane as this is the nvim pane")
-            end
-
-        end
-
-        if pane_candidate then
-            vim.pretty_print(check_if_python(pane_candidate))
-        end
+---Send a ipython cell to connected tmux pane
+M.send_cell = function()
+    if not M.pane then
+        vim.api.nvim_err_writeln("Please call connect first.")
+        return
     end
 
+    if not tmux.check_if_python(M.pane) then
+        vim.api.nvim_err_writeln("Please start python in your connected pane (normally it should happen on connection). Calling disconnect...")
+        M.disconnect()
+        return
+    end
+
+    tmux.send_string(M.pane.id, "test")
+    text.find_previous_comment()
 end
 
-connect_to_tmux(1)
+M.connect()
 
 return M
---
+++ lua/ipython-paste/tmux.lua
@@ -0,0 +1,144 @@
+local M = {}
+
+---Returns the current tmux info
+---@return string?
+M.get_tmux = function()
+    return os.getenv("TMUX")
+end
+
+
+---Get id of active tmux pane
+---@return string?
+M.get_active_pane_id = function()
+    return os.getenv("TMUX_PANE")
+end
+
+---This returns the socket of the current tmux session
+---@return string?
+M.get_socket = function()
+    local tmux_info = M.get_tmux()
+    if tmux_info then
+        return vim.split(tmux_info, ",")[1]
+    end
+end
+
+---This executes a tmux command. "pre" lets you and something before the tmux call
+---@param arg string
+---@param pre string?
+---@return string
+M.execute = function(arg, pre)
+    local command = string.format("%s tmux -S %s %s", pre or "", M.get_socket(), arg)
+
+    local handle = assert(io.popen(command), string.format("unable to execute: [%s]", command))
+    local result = handle:read("*a")
+    handle:close()
+
+    return result
+end
+
+---Check if python is running in a pane
+---@param pane { active: string, index: string, pid: string, cur_cmd: string, id: string }
+---@return boolean
+M.check_if_python = function(pane)
+    if pane.cur_cmd == "python" then
+        return true
+    else
+        return false
+    end
+end
+
+---Get information about all panes in current tmux window
+---cmd_out is a string which is specifcally formatted
+---The format can be seen under |get_pane|
+---
+---@param cmd_out string
+---@return { active: string, index: string, pid: string, cur_cmd: string, id: string }[]
+M.get_pane_infos = function(cmd_out)
+    local pane_infos = {}
+
+    for _, pane_info in pairs(vim.split(cmd_out, "\n")) do
+        local info_splitted = vim.split(pane_info, " ")
+        if #info_splitted > 1 then
+            local index = tonumber(info_splitted[2])
+            local info_table = {
+                active = info_splitted[1],
+                index = info_splitted[2],
+                pid = info_splitted[3],
+                cur_cmd = info_splitted[4],
+                id = info_splitted[5]
+            }
+
+            if index then
+                table.insert(pane_infos, index + 1, info_table)
+            end
+        end
+    end
+    return pane_infos
+end
+
+---Returns the via pane_num specified pane or if only 2 panes are currently there the not active one
+---@param pane_num number?
+---@return { active: string, index: string, pid: string, cur_cmd: string, id: string } | nil
+M.get_pane = function(pane_num)
+    if pane_num then
+        pane_num = pane_num + 1
+    end
+
+    if M.get_tmux() then
+        local cmd_out = M.execute("list-panes -F '#{pane_active} #{pane_index} #{pane_pid} #{pane_current_command} #{pane_id}'")
+
+        local pane_candidate = nil
+
+        local pane_infos = M.get_pane_infos(cmd_out)
+
+        if #pane_infos == 2 then
+            for _, pane_info in pairs(pane_infos) do
+                if pane_info.active == "0" then
+                    pane_candidate = pane_info
+                end
+            end
+        else
+            if pane_num == nil then
+                vim.api.nvim_err_writeln("Please specify the tmux pane number (to check use tmux-prefix + q)")
+                return nil
+            end
+            if pane_infos[pane_num].active ~= 1 then
+                pane_candidate = pane_infos[pane_num]
+            else
+                vim.api.nvim_err_writeln("pane num cannot be the active pane as this is the nvim pane")
+                return nil
+            end
+        end
+
+        if pane_candidate then
+            return pane_candidate
+        end
+        return nil
+    end
+end
+
+
+---Send a string to a tmux pane and press enter to for example execute
+---@param pane_id string
+---@param str string
+M.send_string_enter = function(pane_id, str)
+    local cmd = str.format("send-keys -t '%s' '%s' Enter", pane_id, str)
+    M.execute(cmd)
+end
+
+---Send a string to a tmux pane
+---@param pane_id string
+---@param str string
+M.send_string = function(pane_id, str)
+    local cmd = string.format("send-keys -t '%s' '%s'", pane_id, str)
+    M.execute(cmd)
+end
+
+---
+---@param pane { active: string, index: string, pid: string, cur_cmd: string, id: string }
+---@param python_command any
+M.run_python = function(pane, python_command)
+    M.send_string_enter(pane.id, python_command)
+end
+
+return M
